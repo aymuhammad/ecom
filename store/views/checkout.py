a1acf1cb99f5da87c7de0cbd6a5d58204d45fc60
Auwal Yahaya <auwalyah@yahoo.com>
2023-01-07 22:44:28 +0100

    checkout view

--- store/views/checkout.py
+++ store/views/checkout.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.hashers import check_password
 from django.shortcuts import redirect, render
 from django.views import View
-from store.models.customer import Customer
-from store.models.orders import Order
-from store.models.products import Products
+from store.models import Customer
+from store.models import Order
+from store.models import Products
 
 
 class CheckOut(View):
