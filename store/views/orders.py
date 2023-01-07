7e66cfb6bc63cb50158b6c80aab0240feec39306
Auwal Yahaya <auwalyah@yahoo.com>
2023-01-07 22:51:14 +0100

    order view

--- store/views/orders.py
+++ store/views/orders.py
@@ -2,9 +2,9 @@ from django.contrib.auth.hashers import check_password
 from django.shortcuts import redirect, render
 from django.views import View
 from store.middlewares.auth import auth_middleware
-from store.models.customer import Customer
-from store.models.orders import Order
-from store.models.products import Products
+from store.models import Customer
+from store.models import Order
+from store.models import Products
 
 
 class OrderView(View):
